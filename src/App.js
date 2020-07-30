0908210d5e795b30543eabdb090d03ab53133086
Adrian Gora <adrian.gora14@gmail.com>
2020-07-30 19:39:18 +0200

    styled components update, Coins.css got delted by git

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Coin from './components/Coin/Coin';
-
+import AccountBalance from './components/AccountBalance/AccountBalance.jsx';
 
 
 
@@ -15,6 +15,7 @@ function App() {
           Coin Exchange
         </h1>
       </header>
+      <AccountBalance amount={1000} />
       <table className="coin-table">
       <thead>
         <tr>
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
-import './Coin.css';
 import propTypes from 'prop-types';
+import styled from 'styled-components';
+
+
+
+const Td = styled.td`
+    border: 1px solid #cccccc;
+    width: 25vh;
+
+`;
 
 export default class Coin extends Component {
     constructor(props) {
@@ -36,15 +44,15 @@ export default class Coin extends Component {
    }
     render() {
         return (
-           <tr className="coin-row">
-               <td>{this.props.name}</td>
-               <td>{this.props.ticker}</td>
-               <td>${this.state.price}</td>
-               <td>
-                   <form action="#" method="POST">
-                    <button onClick={this.handleClick}>Refresh</button>
-                   </form>
-                </td>
+           <tr>
+               <Td>{this.props.name}</Td>
+               <Td>{this.props.ticker}</Td>
+               <Td>${this.state.price}</Td>
+               <Td>
+                    <form action="#" method="POST">
+                        <button onClick={this.handleClick}>Refresh</button>
+                    </form>
+               </Td> 
            </tr>
         );
     }
