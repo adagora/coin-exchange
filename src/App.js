6238b9e3d297f5416ae41e57580af958b86808f6
Adrian Gora <adrian.gora14@gmail.com>
2020-08-10 14:24:51 +0200

    Hide/Show Balance

--- src/App.js
+++ src/App.js
@@ -16,34 +16,50 @@ class App extends React.Component {
     super(props);
     this.state = {
       balance: 10000,
+      showBalance: true,
       coinData: [
         {
           name: 'Bitcoin',
           ticker: 'BTC',
-          price: 9999.99
+          price: 9999.99,
+          balance: 1
         },
         {
           name: 'Ethereum',
           ticker: 'ETH',
-          price: 300
+          price: 300,
+          balance: 3
         },
         {
           name: 'Tether',
           ticker: 'USDT',
-          price: 1.0
+          price: 1.0,
+          balance: 0.5
         },
         {
           name: 'Cardano',
           ticker: 'ADA',
-          price: 0.14
+          price: 0.14,
+          balance: 10000
         },
       ]
     }
     this.handleRefresh = this.handleRefresh.bind(this);
+    this.handleBalanceVisibilityChange = this.handleBalanceVisibilityChange.bind(this);
+  }
+  
+  handleBalanceVisibilityChange(){
+    this.setState( function(oldState) {
+      return {
+        ...oldState,
+        showBalance: !oldState.showBalance
+      }
+    });
+
   }
   // passing down this event Handler as props and calling event Handler in child component and passing information back to the parent
   handleRefresh(valueChangeTicker) {
-    const newCoinData = this.state.coinData.map(function( {ticker, name, price} ) {
+    const newCoinData = this.state.coinData.map(function( {ticker, name, price, balance} ) {
       let newPrice = price;
       if ( valueChangeTicker === ticker ) {
         const randomPercentage = 0.995 + Math.random() * 0.01;
@@ -53,6 +69,8 @@ class App extends React.Component {
           ticker,
           name,
           price: newPrice,
+          balance
+  
         }
     });
 
@@ -63,8 +81,14 @@ class App extends React.Component {
     return (
       <Div className="App">
         <ExchangeHeader />
-        <AccountBalance amount={this.state.balance} />
-        <CoinList coinData={this.state.coinData} handleRefresh={this.handleRefresh} />
+        <AccountBalance 
+          amount={this.state.balance} 
+          showBalance={this.state.showBalance} 
+          handleBalanceVisibilityChange={this.handleBalanceVisibilityChange} />
+        <CoinList 
+          coinData={this.state.coinData}
+          showBalance={this.state.showBalance} 
+          handleRefresh={this.handleRefresh} />
       </Div>
     );
   }
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -22,14 +22,6 @@ export default class Coin extends Component {
 
         //submitting (this.props.ticker) to the parent to find value
        this.props.handleRefresh(this.props.ticker);
-/*
-       const randomPercentage = 0.995 + Math.random() * 0.01;
-       this.setState( oldState => {
-        return {
-            price: oldState.price * randomPercentage
-        };
-       });
-    */  
    }
     render() {
         return (
@@ -37,6 +29,7 @@ export default class Coin extends Component {
                <Td>{this.props.name}</Td>
                <Td>{this.props.ticker}</Td>
                <Td>${this.props.price}</Td>
+               {this.props.showBalance ?<Td>${this.props.balance}</Td> : null}
                <Td>
                     <form action="#" method="POST">
                         <button onClick={this.handleClick}>Refresh</button>
