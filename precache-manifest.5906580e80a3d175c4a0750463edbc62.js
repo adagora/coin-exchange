b093a47925f47156ab3581dbee9039577ed3f8b1
Adrian Gora <adrian.gora14@gmail.com>
2020-08-17 23:21:52 +0200

    Updates

---
+++ precache-manifest.5906580e80a3d175c4a0750463edbc62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27eab9b3406ded86be45de79f32223c",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "0750a619b946d60846c5",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa3348301182527cfc6e",
+    "url": "/coin-exchange/static/js/2.e9423c8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.e9423c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0750a619b946d60846c5",
+    "url": "/coin-exchange/static/js/main.7c5c6172.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
